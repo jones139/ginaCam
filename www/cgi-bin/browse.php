0491892ce0707a76bba25ba92c393673a144a78f
jones139 <grahamjones139@gmail.com>
2012-07-24 21:41:40 +0100

    Got GalleryView working, but it seems to crash if you have too many images - only ~200 crashes it - will try a different library before I try to fix GalleryView

--- www/cgi-bin/browse.php
+++ www/cgi-bin/browse.php
@@ -3,16 +3,13 @@
 include('config.php');
 $camId = $_GET['camid'];
 $subDir = $_GET['subdir'];
+$ajax = $_GET['ajax'];
 
 $realDir = $realBaseDir.$camId.'/'.$subDir;
 $wwwDir = $wwwBaseDir.$camId.'/'.$subDir;
 
-echo '<link rel="stylesheet" type="text/css" href="'.$wwwBaseDir.'/css/webcam.css" />';
+$retObj = array();
 
-echo "<h1>Browsing Camera ".$camId."/".$subDir."</h1>";
-echo '<p><a href="'.$wwwBaseDir.'">Home</a>'
-      .':<a href="'.$wwwBaseDir.'/cgi-bin/browse.php?camid='.$camId.'">'.$camId.'</a>'
-      .'</p>';
 
 
 # Scan Directory
@@ -23,21 +20,21 @@ if (!is_dir($realDir)) {
     $fileList = scandir($realDir);  
 
     #First list Directories...
-    echo "<h2>Directories</h2>";
+    $dirArr = array();
     for($i = 0; $i<count($fileList); $i++) {
     	   $entry = $fileList[$i];
            if ($entry != "." && $entry != "..") {
               if (is_dir($realDir.'/'.$entry)) {
-               	 echo '<a href="'.$wwwBaseDir.'/cgi-bin/browse.php?camid='.$camId.
-		      '&subdir='.$subDir.'/'.$entry.'">'.$entry.'</a><br/>';
+	         $dirURL = $wwwBaseDir.'/cgi-bin/browse.php?camid='.$camId.
+		      '&subdir='.$subDir.'/'.$entry;
+	         $dirArr[]=$dirURL;
               }
     	   }
     }
-
+    $retObj['directories']=$dirArr;
 
     #Now list animated gif video files
-    echo "<h2>Videos</h2>";
-
+    $vidArr = array();
     for ($i=0; $i<count($fileList); $i++)
     {
 	$fileParts = explode(".",$fileList[$i]);
@@ -46,12 +43,14 @@ if (!is_dir($realDir)) {
 	if (in_array($fileExt,$validExts)) {
 	   $realFile = $realDir.'/'.$fileList[$i];
 	   $wwwFile = $wwwDir . substr($realFile,strlen($realDir));
-	   echo '<a href="'.$wwwFile.'">'.$fileList[$i].'</a><br/>';
+	   #echo '<a href="'.$wwwFile.'">'.$fileList[$i].'</a><br/>';
+	   $vidArr[]=$wwwFile;
 	}
     }
+    $retObj['videos']=$vidArr;
 
     #Now list image files
-    echo "<h2>Images<h2/>";
+    $imgArr = array();
     for ($i=0; $i<count($fileList); $i++)
     {
 	$fileParts = explode(".",$fileList[$i]);
@@ -60,11 +59,78 @@ if (!is_dir($realDir)) {
 	if (in_array($fileExt,$validExts)) {
 	   $realFile = $realDir.'/'.$fileList[$i];
 	   $wwwFile = $wwwDir . substr($realFile,strlen($realDir));
-	   echo '<a href="'.$wwwFile.'">';
-	   echo '<img class="thumbnail" src="'.$wwwFile.'" alt="Snapshot Image">';
-	   echo '</a>';
-	   echo "&nbsp;&nbsp;";
+	   #echo '<a href="'.$wwwFile.'">';
+	   #echo '<img class="thumbnail" src="'.$wwwFile.'" alt="Snapshot Image">';
+	   #echo '</a>';
+	   #echo "&nbsp;&nbsp;";
+	   $imgArr[] = $wwwFile;
 	}
+    }
+    $retObj['images']=$imgArr;
+
+    if (isset($ajax)) {
+        print json_encode($retObj);
+    } else {
+      echo '<link rel="stylesheet" type="text/css" href="'
+	.$wwwBaseDir.'/css/webcam.css" />';
+      echo '<script src="'.$wwwBaseDir.'/js/jquery-1.7.2.min.js"></script>';
+      echo '<script src="'.$wwwBaseDir.'/js/jquery-ui-1.8.21.custom.min.js"></script>';
+      echo '<script type="text/javascript" src="'.$wwwBaseDir.'/js/jquery.timers-1.2.js"></script>';
+      echo '<script type="text/javascript" src="'.$wwwBaseDir
+	.'/js/jquery.easing.1.3.js"></script>';
+      echo '<script type="text/javascript" src="'.$wwwBaseDir
+	.'/js/jquery.galleryview-3.0-dev.js"></script>';
+      echo '<link type="text/css" rel="stylesheet" href="'.$wwwBaseDir
+	.'/css/jquery.galleryview-3.0-dev.css" />';
+
+      echo '<script type="text/javascript">'
+	.'$(function(){'
+	.'	$("#imgGallery").galleryView({'
+	.'      transition_speed:1000,'
+	.'      transition_interval:500,'
+	.'      panel_animation:"fade",'
+	.'      autoplay:true'
+	.'      });'
+	.'});'
+	.'</script>';
+
+
+      echo "<h1>Browsing Camera ".$camId."/".$subDir."</h1>";
+      echo '<p><a href="'.$wwwBaseDir.'">Home</a>'
+	.':<a href="'.$wwwBaseDir.'/cgi-bin/browse.php?camid='.$camId.'">'.$camId.'</a>'
+	.'</p>';
+
+      echo "<h2>Directories</h2>";
+      echo "<ul>";
+      for ($i = 0; $i<count($retObj['directories']); $i++) {
+	$dirURL = $retObj['directories'][$i];
+	$urlParts = (explode("&subdir=",$dirURL));
+	$dirName = $urlParts[1];
+	echo '<li><a href="'.$dirURL.'">'.$dirName.'</a></li>';
+      }
+      echo "</ul>";
+      echo "<h2>Videos</h2>";
+      echo "<ul>";
+      for ($i = 0; $i<count($retObj['videos']); $i++) {
+	$vidURL = $retObj['videos'][$i];
+	$urlParts = (explode("/",$vidURL));
+	$vidFname = $urlParts[count($urlParts)-1];
+	echo '<li><a href="'.$vidURL.'">'.$vidFname.'</a></li>';
+      }
+      echo "</ul>";
+
+      echo "<h2>Images<h2/>";
+      echo "<ul id='imgGallery'>";
+      for ($i = 0; $i<count($retObj['images']); $i++) {
+	$imgURL = $retObj['images'][$i];
+	$urlParts = (explode("/",$imgURL));
+	$imgFname = $urlParts[count($urlParts)-1];
+	echo '<li>'
+	  .'<img class="thumbnail" src="'
+	  .$imgURL.'" alt="image..."></li>';
+      }
+      echo "</ul>";
+
     }
 
 }
